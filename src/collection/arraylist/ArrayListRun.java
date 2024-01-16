d77d44ec86726c56836b89194adde5fbd90fee11
AsheS <campus265@gmail.com>
2024-01-16 20:51:29 +0700

    Initial commit

---
+++ src/collection/arraylist/ArrayListRun.java
@@ -0,0 +1,14 @@
+package collection.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListRun {
+    public static void main(String[] args) {
+        ArrayList<String> strs = new ArrayList<>();
+        strs.add("A");
+        strs.add("B");
+        System.out.println(strs);
+    }
+
+
+}
